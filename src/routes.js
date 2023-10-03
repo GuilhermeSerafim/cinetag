feb527af9d4db6839fcff949adfee7db3ec1c952
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-03 09:47:05 -0300

    Deixando o cabecalho e o rodape para qualquer rota no routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
 import Inicio from "pages/Inicio";
+import Favoritos from "pages/Inicio/Favoritos";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
+            <Cabecalho />
             <Routes>
                 <Route path="/" element={<Inicio />}></Route>
+                <Route path="/favoritos" element={<Favoritos />} />
             </Routes>
+            <Rodape />
         </BrowserRouter>
     )
 }
