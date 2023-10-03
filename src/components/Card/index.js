64cb725a210dc60112d4631a1ca1353b5d131cd4
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-03 11:20:11 -0300

    Adicionando uma lógica na exibição dos icones (parte visual)

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,21 @@
 import { useFavoritoContext } from "contextos/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from './favoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
 
 
 function Card({ id, capa, titulo }) {
     const { favorito, adicionarFavorito } = useFavoritoContext();
+    //Parte visual
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+    
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
             <img
-                src={iconeFavoritar}
+                src={icone}
                 alt="Favoritar filme"
                 className={styles.favoritar}
                 onClick={() => {
