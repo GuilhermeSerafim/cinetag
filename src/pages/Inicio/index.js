592a4f643ffa85ae09fc7259370afdc2cbdcb190
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-03 09:37:01 -0300

    Criando lista dinamica

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from "json/db.json"
+import styles from "./Inicio.module.css"
 
 function Inicio() {
     return (
@@ -12,7 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes</h1>
             </Titulo>
-            <Card id='1' titulo='Gato' capa='https://conteudo.imguol.com.br/c/entretenimento/0a/2023/06/29/gato-do-deserto-1688070337093_v2_3x4.jpg'/>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
