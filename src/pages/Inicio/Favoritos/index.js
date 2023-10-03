59318b3945bcf7323cee6746eca2d71e82bdec2a
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-03 09:56:13 -0300

    Criando page dos meus favoritos, estático por enquanto

--- src/pages/Inicio/Favoritos/index.js
+++ src/pages/Inicio/Favoritos/index.js
@@ -1,8 +1,18 @@
+import Banner from 'components/Banner';
 import styles from './Favoritos.module.css';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
 
 function Favoritos() {
     return (
         <>
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id='1' titulo='Gato bonifacio' capa='https://queirosiana.files.wordpress.com/2021/02/banho-em-gato-angora-valores-1.jpg' />
+            </section>
         </>
     )
 }
